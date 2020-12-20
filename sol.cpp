144ea1a9b6f902857920bed06ccf4c18a81c07e4
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-20 20:28:34 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -7,14 +7,14 @@ using namespace std;
 void solve() {
 	string s;
 	cin >> s;
-  //f(x) is a function where you reverse the value and omit the leading zeroes
-  //for example, f(100) = 1
-  //g(x) = x/f(f(x))
-  //for example, g(100) = 100/f(f(100))
-  //g(100) = 100/f(1)
-  //g(100) = 100/1
-  //therefore, g(100) = 100
-  //as shown above, the number will always be itself so just print the size of the string
+	//f(x) is a function where you reverse the value and omit the leading zeroes
+	//for example, f(100) = 1
+	//g(x) = x/f(f(x))
+	//for example, g(100) = 100/f(f(100))
+	//g(100) = 100/f(1)
+	//g(100) = 100/1
+	//therefore, g(100) = 100
+	//as shown above, the number will always be itself so just print the size of the string
 	cout << (ll)s.size() << "\n";
 }
 
