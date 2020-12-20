72f8f99da368cb7c35a3e10c62b19a4facb39d0d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-20 20:29:01 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -9,7 +9,7 @@ void solve() {
 	cin >> s;
 	//f(x) is a function where you reverse the value and omit the leading zeroes
 	//for example, f(100) = 1
-	//g(x) = x/f(f(x))
+	//now we have a function, g(x) = x/f(f(x))
 	//for example, g(100) = 100/f(f(100))
 	//g(100) = 100/f(1)
 	//g(100) = 100/1
